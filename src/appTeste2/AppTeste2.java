e1ab3503cc0ae80b072e8f4845438e6aec425a9b
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-01-10 17:08:50 -0300

    Conclusão da prova interdisciplinar 2

---
+++ src/appTeste2/AppTeste2.java
@@ -0,0 +1,10 @@
+package appTeste2;
+
+public class AppTeste2 {
+
+	public static void main(String[] args) {
+		  Teste2 obj = new Teste2(10);
+		  obj.exibe();
+	}
+
+}
---
+++ src/appTeste2/Teste2.java
@@ -0,0 +1,22 @@
+package appTeste2;
+
+public class Teste2 {
+	private int x;
+	
+	Teste2(int x) {
+		this.x = x;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void exibe() {
+		System.out.println(x);
+	}
+
+}
